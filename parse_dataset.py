128b9f7c34aae36bc67773db1b0b822863cfd558
riemer <riemer@vanderkley.it>
2021-05-24 19:42:02 +0200

    Use pre-allocated array, prevents recopying

--- parse_dataset.py
+++ parse_dataset.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
+import sys
 import argparse
 import pathlib
 from typing import Iterable, List
 import numpy as np
 
 
+def bytes_to_mb(value: int):
+    return round(value / 1024 / 1024, 2)
+
+
 def read_dataset(file: pathlib.Path, delimiter=" ") -> Iterable[List[str]]:
     with open(file) as fh:
         for line in fh:
-            yield line.strip("\n").split(delimiter)
+            yield line.strip().split(delimiter)
 
 
 def numpy_vectorize(file: pathlib.Path) -> np.ndarray:
     """
-    Iterates over the file line-by-line, returns the joined sequence of two flattened arr
+    Iterates over the file, returns the ndarray
     """
-    arr_hex: np.array = np.array([], dtype='<U65')
-    arr_floats: np.array = np.array([], dtype='<f8')
+    # Preallocate arrays (memory efficient)
+    # since for np.append() python needs to make room in the memory again and again for each append
+    entries = 1_000_000  # 1 million entries
+    hex_arr: np.array = np.zeros(entries, dtype='<U65')
+    float_arr: np.array = np.zeros(entries, dtype='<f8')
+    arr = np.stack((hex_arr, float_arr), axis=-1)
 
+    print(f"size of pre-allocated array {bytes_to_mb(arr.nbytes)}mb")
+
+    idx = 0
     for row in read_dataset(file):
         n_rows = int(row[3])
-        _floats, _hex = np.array(row[n_rows * -2:]).reshape(2, n_rows)
-        arr_hex = np.append(arr_hex, _hex)
-        arr_floats = np.append(arr_floats, _floats)
+        _hex, _floats = np.array(row[n_rows * -2:]).reshape(2, n_rows)
 
-    return np.stack((arr_hex, arr_floats), axis=-1)
+        for i in range(0, _hex.size):
+            arr[idx] = _floats[i], _hex[i]
+            idx += 1
+
+    return arr[:idx]
 
 
 def parse_args():
@@ -43,11 +57,15 @@ def main():
     args = parse_args()
     arr = numpy_vectorize(file=args.file)
 
+    print(f"size of array {bytes_to_mb(arr.nbytes)}mb")
+
     result_dict = {}
     for idx, r in enumerate(arr):
         result_dict[r[0]] = (idx, r[1])
 
     print(result_dict)
+
+    print(f"size of result dictionary {bytes_to_mb(sys.getsizeof(result_dict))}mb")
 
 
 if __name__ == "__main__":
